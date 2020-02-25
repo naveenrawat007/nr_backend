24df911be901d698c29d4edd8bc585bd51e5ee23
Vinay Mehta <vinay.ymca@gmail.com>
2020-02-25 16:18:21 +0530

    add routes and jwt decoder and cors setup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,21 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+
+  def authorize_request
+   header = request.headers['Authorization']
+   if header == ""
+     render json: { message: "Empty token", status: 400 ,data: nil}
+   elsif header!=""
+     header = header.split(' ').last if header
+     begin
+       decoded = JWT.decode(header,Rails.application.secrets.secret_key_base,false)
+       @user = User.find(decoded.first['user_id'])
+     rescue ActiveRecord::RecordNotFound => e
+       render json: { message: e.message, status: 401 ,data: nil}
+     rescue JWT::DecodeError => e
+       render json: { message: e.message, status: 500, data: nil }
+     end
+   end
+ end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  namespace 'api' do
+    namespace "v1", constraints: ApiConstraint.new(version: 1) do
+      devise_for :users, controllers: {registrations:"api/v1/devise/users/registrations", sessions:"api/v1/devise/users/sessions", passwords:"api/v1/devise/users/passwords"}
+        devise_scope :user do
+          post "/users/verify_otp" => "devise/users/sessions#otp_verify"
+          post "/users/forget_password" => "devise/users/passwords#forget_password"
+          patch "/users/update_profile" => "devise/users/passwords#update_profile"
+          post "/users/resend_otp" => "devise/users/registrations#resend_otp"
+          get "/user_info" => "devise/users/registrations#get_user_info"
+        end
+    end
+  end
+
 end
