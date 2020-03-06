5b3eb56df072d0b6b2310e695f7260c87054d274
Vinay Mehta <vinay.ymca@gmail.com>
2020-03-06 14:35:15 +0530

    send routine id in notification

--- app/workers/remind_routine_worker.rb
+++ app/workers/remind_routine_worker.rb
@@ -1,14 +1,14 @@
 class RemindRoutineWorker
   include Sidekiq::Worker
 
-  def perform(device_type, device_token, message, title)
+  def perform(device_type, device_token, message, title, id)
     if device_type == "ios"
       APNS.port = 2195
       APNS.pem  = pem
       APNS.pass = ''
       APNS.host = APP_CONFIG['apn_host']
       begin
-        APNS.send_notification(device_token, :alert => { title: title, body: message} , :badge => 1, :sound => 'default')
+        APNS.send_notification(device_token, :alert => { title: title, body: message, id: id} , :badge => 1, :sound => 'default')
       rescue Exception => ex
         return false
       end
@@ -16,7 +16,7 @@ class RemindRoutineWorker
       require 'fcm'
       fcm = FCM.new('AAAA0ppfMK8:APA91bGIDAzeQIXgHILzeC6AxoSMrLUKR4uuG5bWTPAl3YI-_VXN2Ns8UJrHIYaRz3dM4mx9OEHag8-VaAZD7nqcyDQ2TB7wbLCnVY8h7j4MvNuo0GWkjLj8qi_pgzoPmF-qYsuCK4Vv')
       registration_ids = [device_token]
-      options = {data: {title: title, body: message}}
+      options = {data: {title: title, body: message, id: id}}
       fcm.send(registration_ids, options)
     end
   end
