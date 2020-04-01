e3c226fb3b0f6b44452ee2d6903acdddc1552ce6
Vinay Mehta <vinay.ymca@gmail.com>
2020-04-01 10:56:30 +0530

    change mailer and its view

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,8 +2,9 @@ class UserMailer < ApplicationMailer
 
   def otp_send(user)
     @email = user.email
+    @name = user.first_name
     @otp_code = user.otp_code
-    mail(to:user.email, subject: "OTP CODE")
+    mail(to:user.email, subject: "Verify Email")
   end
 
   def feedback(fname,lname,phone,email,query)
