bf5b893f2f9fc7d7ceca5f85945ca244f683176f
Vinay Mehta <vinay.ymca@gmail.com>
2020-04-01 10:25:11 +0530

    change string to int

--- app/controllers/api/v1/routines_controller.rb
+++ app/controllers/api/v1/routines_controller.rb
@@ -7,10 +7,10 @@ module Api
 
       def index
         if params[:weeks].present?
-          routines = @user.routines.where(next_routine_date: (DateTime.now..DateTime.now + ENV["WEEK_COUNT"].weeks),active: true).order(routine_date: :asc).paginate(page: params[:page], per_page: ENV["PER_PAGE"])
+          routines = @user.routines.where(next_routine_date: (DateTime.now..DateTime.now + ENV["WEEK_COUNT"].to_i.weeks),active: true).order(routine_date: :asc).paginate(page: params[:page], per_page: ENV["PER_PAGE"].to_i)
           render json: { message: "Routines", status: 200, routines: ActiveModelSerializers::SerializableResource.new(routines, each_serializer: RoutineSerializer)}
         else
-          routines = @user.routines.paginate(page: params[:page], per_page: ENV["PER_PAGE"]).order(routine_date: :asc)
+          routines = @user.routines.paginate(page: params[:page], per_page: ENV["PER_PAGE"].to_i).order(routine_date: :asc)
           render json: { message: "Routines", status: 200, routines: ActiveModelSerializers::SerializableResource.new(routines, each_serializer: RoutineSerializer)}
         end
       end
